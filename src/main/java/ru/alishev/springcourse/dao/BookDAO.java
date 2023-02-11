a5682bc3acfaf53503177a7ddc59b93cb85a3832
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-02-11 18:41:49 +0300

    DAO, models, repositories

--- src/main/java/ru/alishev/springcourse/dao/BookDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/BookDAO.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
  * @author Neil Alishev
  */
 @Component
-public class PersonDAO {
+public class BookDAO {
     // Здесь будут лежать специфические запросы к БД (с помощью SQL)
 }
--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -1,36 +1,42 @@
 package ru.alishev.springcourse.models;
 
-import javax.validation.constraints.Email;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * @author Neil Alishev
  */
+@Entity
+@Table(name = "Person")
 public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
-    private String name;
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
+    @Column(name = "full_name")
+    private String fullName;
 
-    @Min(value = 0, message = "Age should be greater than 0")
-    private int age;
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
 
-    @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Email should be valid")
-    private String email;
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
 
+    // Конструктор по умолчанию нужен для Spring
     public Person() {
 
     }
 
-    public Person(int id, String name, int age, String email) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
     }
 
     public int getId() {
@@ -41,27 +47,27 @@ public class Person {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFullName() {
+        return fullName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
     }
 
-    public int getAge() {
-        return age;
+    public int getYearOfBirth() {
+        return yearOfBirth;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
     }
 
-    public String getEmail() {
-        return email;
+    public List<Book> getBooks() {
+        return books;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 }
